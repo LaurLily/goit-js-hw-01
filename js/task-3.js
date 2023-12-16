993d0128853746729083e1dcb0003ebaca8d36f5
LaurLily <lilbigliberia@gmail.com>
2023-12-16 17:16:23 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,9 @@ function getElementWidth(content, padding, border) {
     const contentWidth = parseFloat(content);
     const paddingWidth = parseFloat(padding);
     const borderWidth = parseFloat(border);
-    return contentWidth + paddingWidth + borderWidth;
+    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+
+    return totalWidth;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
